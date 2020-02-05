092e206f01a88190b33250af523236a139327ca6
kaizhi du <kaizhidu@kaizhis-MacBook-Pro.local>
2020-02-05 15:36:24 -0800

    Updates

---
+++ precache-manifest.7090e519c211a62e8d12414d24dd880d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64a3a0b9cb55685b6b42dd7d518e4686",
+    "url": "./index.html"
+  },
+  {
+    "revision": "52161222ecf6e042ee52",
+    "url": "./static/js/2.ea586d4b.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "./static/js/2.ea586d4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "014e36902052ea6419b5",
+    "url": "./static/js/main.9645d254.chunk.js"
+  },
+  {
+    "revision": "65adc8f333e4a2bdcce5",
+    "url": "./static/js/runtime-main.018ff082.js"
+  }
+]);
