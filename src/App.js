c111d0cd85e3de751c540099e69b7b8ea0d63c65
kaizhi du <kaizhidu@kaizhis-MacBook-Pro.local>
2020-02-05 15:14:36 -0800

    finish！！！

--- src/App.js
+++ src/App.js
@@ -1,26 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//https://6o0tlfjnq6.execute-api.us-west-2.amazonaws.com/dev/candy
+import React, { useEffect, useState } from 'react';
+import { TextField, Button, Table, TableHead, TableRow, TableCell, TableBody } from "@material-ui/core";
 
-function App() {
+const App = () => {
+
+  const [candies, setCandies] = useState([]);
+  const [newCandy, setNewCandy] = useState({
+    name: '',
+    price: '',
+    number: ''
+  });
+
+  useEffect(() => {
+    fetch('https://6o0tlfjnq6.execute-api.us-west-2.amazonaws.com/dev/candy')
+      .then(res => res.json())
+      .then(data => {
+        setCandies(data);
+      });
+  }, [candies]);
+
+  const handleChange = e => {
+    setNewCandy({
+      ...newCandy,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(newCandy);
+    fetch('https://6o0tlfjnq6.execute-api.us-west-2.amazonaws.com/dev/candy', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newCandy)
+    }).then(r => r.json()).then(data => {
+      console.log(data);
+    });
+    e.target.reset();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          label="name"
+          name="name"
+          onChange={handleChange}
+        />
+        <br/>
+        <TextField
+          label="price"
+          name="price"
+          onChange={handleChange}
+        />
+        <br/>
+        <TextField
+          label="number"
+          name="number"
+          onChange={handleChange}
+        />
+        <br/><br/>
+        <Button type='submit' variant="contained" color="primary">Add</Button>
+      </form>
+      <br/>
+
+      <Table stickyHeader aria-label="sticky table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell>Price</TableCell>
+            <TableCell>Number</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {candies.map(candy => {
+            return <TableRow key={candy.id}>
+              <TableCell>{candy.name}</TableCell>
+              <TableCell>{candy.price}</TableCell>
+              <TableCell>{candy.number}</TableCell>
+            </TableRow>;
+          })}
+        </TableBody>
+
+      </Table>
+
+
+    </>
   );
-}
+};
 
 export default App;
